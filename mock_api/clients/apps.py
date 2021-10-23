9078946a90ab5fb0d18420f242c5eba6579a2a6a
Hernan Diaz <hernandiaz035@gmail.com>
2021-10-23 14:40:08 +1100

    Create Clients app with necessary files.

---
+++ mock_api/clients/apps.py
@@ -0,0 +1,9 @@
+"""Clients App."""
+
+from django.apps import AppConfig
+
+class ClientAppConfig(AppConfig):
+    """Clients AppConfig."""
+
+    name = 'mock_api.clients'
+    verbose_name = 'Clients'
