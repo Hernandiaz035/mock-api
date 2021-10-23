8c2645eef96269c62a0e8fd693395e70b2052002
Hernan Diaz <hernandiaz035@gmail.com>
2021-10-23 12:28:29 +1100

    Initial Comit

---
+++ mock_api/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from mock_api.users.models import User
+from mock_api.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
